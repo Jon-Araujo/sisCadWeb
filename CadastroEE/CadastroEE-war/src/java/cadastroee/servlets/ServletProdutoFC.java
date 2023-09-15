1e2f3c93f89349f57c7c94e5c0457d3c3b810117
Jonathan Araujo <jonathan.araujo76@icloud.com>
2023-09-15 15:12:57 -0300

    Projeto finalizado.

---
+++ CadastroEE/CadastroEE-war/src/java/cadastroee/servlets/ServletProdutoFC.java
@@ -0,0 +1,157 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package cadastroee.servlets;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.ejb.EJB;
+import cadastroee.controller.ProdutosFacadeLocal;
+import cadastroee.model.Produtos;
+import java.util.List;
+@WebServlet(name = "ServletProdutoFC", urlPatterns = {"/ServletProdutoFC"})
+public class ServletProdutoFC extends HttpServlet {
+    @EJB
+    ProdutosFacadeLocal facade;   
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {        
+    }
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String acao = request.getParameter("acao");
+        String destino = null;
+
+        switch (acao) {
+            case "listar":
+                List<Produtos> produtos = facade.findAll();
+                request.setAttribute("produtos", produtos);
+                destino = "ProdutoLista.jsp";
+                break;
+            case "formIncluir":
+                destino = "ProdutoDados.jsp";
+                break;
+            case "formAlterar":
+                int idAlterar = Integer.parseInt(request.getParameter("id"));
+                Produtos produtoAlterar = facade.find(idAlterar);
+                request.setAttribute("produto", produtoAlterar);            
+                destino = "ProdutoDados.jsp";
+                break;
+            case "excluir":
+                int idExcluir = Integer.parseInt(request.getParameter("id"));
+                Produtos produtoExcluir = facade.find(idExcluir);
+                facade.remove(produtoExcluir);
+                List<Produtos> produtosExcluidos = facade.findAll();
+                request.setAttribute("produtos", produtosExcluidos);
+                destino = "ProdutoLista.jsp";
+                break;
+            default:
+                break;
+        }
+        RequestDispatcher rd = request.getRequestDispatcher(destino);
+        rd.forward(request, response); 
+    }
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String acao = request.getParameter("acao");
+        List<Produtos> produtos = null;
+
+        switch (acao) {
+            case "incluir":
+                String nome = request.getParameter("nome");
+                int quantidade = Integer.parseInt(request.getParameter("quantidade"));
+                float preco = Float.parseFloat(request.getParameter("preco"));
+                produtos = facade.findAll();
+                int ultimoId = 0;              
+                Produtos ultimoProduto = produtos.get(produtos.size() - 1);
+                ultimoId = ultimoProduto.getIdProduto();                
+                int id = ultimoId + 1;
+                
+                Produtos novoProduto = new Produtos();
+                novoProduto.setNome(nome);
+                novoProduto.setQuantidade(quantidade);
+                novoProduto.setPrecoVenda(preco);
+                novoProduto.setIdProduto(id);
+
+                facade.create(novoProduto);
+
+                produtos = facade.findAll();
+                request.setAttribute("produtos", produtos);
+                
+                break;
+            
+            case "alterar":
+                int idAlterar = Integer.parseInt(request.getParameter("id"));
+                Produtos produtoExistente = facade.find(idAlterar);
+
+                if (produtoExistente != null) {
+                    String novoNome = request.getParameter("nome");
+                    String novaQuantidadeStr = request.getParameter("quantidade");
+                    String novoPrecoStr = request.getParameter("preco");
+
+                    int novaQuantidade = 
+                            (novaQuantidadeStr != null && !novaQuantidadeStr.isEmpty())
+                            ? Integer.parseInt(novaQuantidadeStr) : produtoExistente.getQuantidade();
+                    float novoPreco = 
+                            (novoPrecoStr != null && !novoPrecoStr.isEmpty()) 
+                            ? Float.parseFloat(novoPrecoStr) : produtoExistente.getPrecoVenda();
+
+                    produtoExistente.setNome(novoNome);
+                    produtoExistente.setQuantidade(novaQuantidade);
+                    produtoExistente.setPrecoVenda(novoPreco);
+
+                    facade.edit(produtoExistente);
+                    produtos = facade.findAll();
+                    request.setAttribute("produtos", produtos);
+                } else {
+                    produtos = facade.findAll();
+                    request.setAttribute("produtos", produtos);
+                }
+                break; 
+        }
+        RequestDispatcher rd = request.getRequestDispatcher("ProdutoLista.jsp");
+        rd.forward(request, response);
+}
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
---
+++ CadastroEE/CadastroEE-war/web/ProdutoLista.jsp
@@ -0,0 +1,42 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+    "http://www.w3.org/TR/html4/loose.dtd">
+  <html>
+        <head>
+            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+            <title>Lista de Produtos</title>
+        </head>
+        <body class="container">
+            <h1>Listagem de Produtos</h1>
+    <a class="btn btn-primary  m-2" href="ServletProdutoFC?acao=formIncluir">Novo Produto</a>
+    <table class="table table-striped" border="1">
+        <tr class="table-dark">
+            <th>#</th>
+            <th>Nome</th>
+            <th>Quantidade</th>
+            <th>Preço</th>
+            <th>Opções</th>
+        </tr>
+        <!-- Aqui virá a lista de produtos -->
+        <%-- Loop para exibir a lista de produtos --%>
+        <c:forEach items="${produtos}" var="produto">
+    <tr>
+        <td>${produto.idProduto}</td>
+        <td>${produto.nome}</td>
+        <td>${produto.quantidade}</td>
+        <td>${produto.precoVenda}</td>
+        <td>
+            <a class="btn btn-primary  btn-sm" href="ServletProdutoFC?acao=formAlterar&id=${produto.idProduto}">Alterar</a>
+            <a class="btn btn-danger  btn-sm" href="ServletProdutoFC?acao=excluir&id=${produto.idProduto}"">Excluir</a>
+        </td>
+    </tr>
+    </c:forEach>
+   </table>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+  </body>
+</html>
+
